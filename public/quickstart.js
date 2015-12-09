90d50700d986ee9c5e87076bf42f2ac62c6f20d3
sraval <sraval@twilio.com>
2015-12-09 01:12:53 -0800

    fix

--- public/quickstart.js
+++ public/quickstart.js
@@ -1,4 +1,3 @@
-
 var conversationsClient;
 var activeConversation;
 var previewMedia;
@@ -10,50 +9,50 @@ if (!navigator.webkitGetUserMedia && !navigator.mozGetUserMedia) {
 
 $.getJSON('/token', function(data) {
 
-var accessToken = data.token;
-var identity = data.identity;
-var accessManager = new Twilio.AccessManager(accessToken);
+    var accessToken = data.token;
+    var identity = data.identity;
+    var accessManager = new Twilio.AccessManager(accessToken);
 
-//Check the browser console to see your generated identity. Send an invite to yourself if you want! 
-console.log(identity);
+    //Check the browser console to see your generated identity. Send an invite to yourself if you want! 
+    console.log(identity);
 
-// create a Conversations Client and connect to Twilio
-conversationsClient = new Twilio.Conversations.Client(accessManager);
-conversationsClient.listen().then(
-  clientConnected,
-  function (error) {
-    log('Could not connect to Twilio: ' + error.message);
-  }
+    // create a Conversations Client and connect to Twilio
+    conversationsClient = new Twilio.Conversations.Client(accessManager);
+    conversationsClient.listen().then(
+    clientConnected,
+    function (error) {
+        log('Could not connect to Twilio: ' + error.message);
+    }
 );
 
 // successfully connected!
 function clientConnected() {
-  document.getElementById('invite-controls').style.display = 'block';
-  log("Connected to Twilio. Listening for incoming Invites as '" + conversationsClient.identity + "'");
+    document.getElementById('invite-controls').style.display = 'block';
+    log("Connected to Twilio. Listening for incoming Invites as '" + conversationsClient.identity + "'");
 
-  conversationsClient.on('invite', function (invite) {
-    log('Incoming invite from: ' + invite.from);
-    invite.accept().then(conversationStarted);
-  });
+    conversationsClient.on('invite', function (invite) {
+        log('Incoming invite from: ' + invite.from);
+        invite.accept().then(conversationStarted);
+    });
 
-  // bind button to create conversation
-  document.getElementById('button-invite').onclick = function () {
-    var inviteTo = document.getElementById('invite-to').value;
+    // bind button to create conversation
+    document.getElementById('button-invite').onclick = function () {
+        var inviteTo = document.getElementById('invite-to').value;
 
-    if (activeConversation) {
-      // add a participant
-      activeConversation.invite(inviteTo);
-    } else {
-      // create a conversation
-      var options = {};
-      if (previewMedia) {
-        options.localMedia = previewMedia;
-      }
-      conversationsClient.createConversation(inviteTo, options).then(
+        if (activeConversation) {
+        // add a participant
+        activeConversation.invite(inviteTo);
+        } else {
+        // create a conversation
+        var options = {};
+        if (previewMedia) {
+            options.localMedia = previewMedia;
+        }
+        conversationsClient.createConversation(inviteTo, options).then(
         conversationStarted,
         function (error) {
-          log('Unable to create conversation');
-          console.error('Unable to create conversation', error);
+            log('Unable to create conversation');
+            console.error('Unable to create conversation', error);
         }
       );
     }
@@ -62,50 +61,50 @@ function clientConnected() {
 
 // conversation is live
 function conversationStarted(conversation) {
-  log('In an active Conversation');
-  activeConversation = conversation;
-  // draw local video, if not already previewing
-  if (!previewMedia) {
-    conversation.localMedia.attach('#local-media');
-  }
-  // when a participant joins, draw their video on screen
-  conversation.on('participantConnected', function (participant) {
-    log("Participant '" + participant.identity + "' connected");
-    participant.media.attach('#remote-media');
-  });
-  // when a participant disconnects, note in log
-  conversation.on('participantDisconnected', function (participant) {
-    log("Participant '" + participant.identity + "' disconnected");
-  });
-  // when the conversation ends, stop capturing local video
-  conversation.on('ended', function (conversation) {
-    log("Connected to Twilio. Listening for incoming Invites as '" + conversationsClient.identity + "'");
-    conversation.localMedia.stop();
-    conversation.disconnect();
-    activeConversation = null;
-  });
+    log('In an active Conversation');
+    activeConversation = conversation;
+    // draw local video, if not already previewing
+    if (!previewMedia) {
+        conversation.localMedia.attach('#local-media');
+    }
+    // when a participant joins, draw their video on screen
+    conversation.on('participantConnected', function (participant) {
+        log("Participant '" + participant.identity + "' connected");
+        participant.media.attach('#remote-media');
+    });
+    // when a participant disconnects, note in log
+    conversation.on('participantDisconnected', function (participant) {
+        log("Participant '" + participant.identity + "' disconnected");
+    });
+    // when the conversation ends, stop capturing local video
+    conversation.on('ended', function (conversation) {
+        log("Connected to Twilio. Listening for incoming Invites as '" + conversationsClient.identity + "'");
+        conversation.localMedia.stop();
+        conversation.disconnect();
+        activeConversation = null;
+    });
 };
 
 //  local video preview
 document.getElementById('button-preview').onclick = function () {
-  if (!previewMedia) {
-    previewMedia = new Twilio.Conversations.LocalMedia();
-    Twilio.Conversations.getUserMedia().then(
-      function (mediaStream) {
-        previewMedia.addStream(mediaStream);
-        previewMedia.attach('#local-media');
-      },
-      function (error) {
-        console.error('Unable to access local media', error);
-        log('Unable to access Camera and Microphone');
-      }
+    if (!previewMedia) {
+        previewMedia = new Twilio.Conversations.LocalMedia();
+        Twilio.Conversations.getUserMedia().then(
+        function (mediaStream) {
+            previewMedia.addStream(mediaStream);
+            previewMedia.attach('#local-media');
+        },
+        function (error) {
+            console.error('Unable to access local media', error);
+            log('Unable to access Camera and Microphone');
+        }
     );
   };
 };
 
 // activity log
 function log(message) {
-  document.getElementById('log-content').innerHTML = message;
+    document.getElementById('log-content').innerHTML = message;
 };
 
-  });
+});
