972f4e32638eb33afba897029466756e66b5dc5f
Jozsef Vass <jvass@twilio.com>
2017-04-25 14:13:39 -0700

    change it to a token server

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# Video Quickstart for Node.js
+# Video Access Token Server for Node.js
 
-This application should give you a ready-made starting point for writing your
-own video apps with Twilio Video. Before we begin, we need to collect
+This server-side application demonstrates generating Access Token for Twilio Video.
+Before we begin, we need to collect
 all the config values we need to run the application:
 
 | Config Value  | Description |
 | :-------------  |:------------- |
-Configuration Profile SID | Identifier for a set of config properties for your video application - [find yours here](https://www.twilio.com/console/video/profiles).
 Account SID | Your primary Twilio account identifier - find this [in the console here](https://www.twilio.com/console).
 API Key | Used to authenticate - [generate one here](https://www.twilio.com/console/video/dev-tools/api-keys).
 API Secret | Used to authenticate - [just like the above, you'll get one here](https://www.twilio.com/console/video/dev-tools/api-keys).
@@ -39,12 +38,8 @@ Now we should be all set! Run the application using the `node` command.
 node .
 ```
 
-Your application should now be running at [http://localhost:3000](http://localhost:3000). Just enter
-the name of the room you want to join and click on 'Join Room'. Then,
-open another tab and join the same room. Now, you should see your own
-video in both the tabs!
+To generate Access Token, visit [http://localhost:3000?identity=alice&room=example](http://localhost:3000?identity=alice&room=example).
 
-![screenshot of chat app](https://s3.amazonaws.com/com.twilio.prod.twilio-docs/images/video2.original.png)
 
 ## License
 MIT
--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-/*
-Load Twilio configuration from .env config file - the following environment
-variables should be set:
-process.env.TWILIO_ACCOUNT_SID
-process.env.TWILIO_API_KEY
-process.env.TWILIO_API_SECRET
-process.env.TWILIO_CONFIGURATION_SID
-*/
 require('dotenv').load();
 var http = require('http');
-var path = require('path');
-var AccessToken = require('twilio').AccessToken;
-var VideoGrant = AccessToken.VideoGrant;
 var express = require('express');
-var randomUsername = require('./randos');
+
+var AccessToken = require('twilio').jwt.AccessToken;
+var VideoGrant = AccessToken.VideoGrant;
 
 // Create Express webapp
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-Generate an Access Token for a chat application user - it generates a random
-username for the client requesting a token, and takes a device ID as a query
-parameter.
-*/
-app.get('/token', function(request, response) {
-    var identity = randomUsername();
-    
+app.get('/', function(request, response) {
     // Create an access token which we will sign and return to the client,
     // containing the grant we just created
     var token = new AccessToken(
@@ -34,19 +17,16 @@ app.get('/token', function(request, response) {
         process.env.TWILIO_API_SECRET
     );
 
-    // Assign the generated identity to the token
-    token.identity = identity;
+    // Assign identity to the token
+    token.identity = request.query.identity || 'identity';
 
-    //grant the access token Twilio Video capabilities
+    // Grant the access token Twilio Video capabilities
     var grant = new VideoGrant();
-    grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+    grant.room = request.query.room;
     token.addGrant(grant);
 
-    // Serialize the token to a JWT string and include it in a JSON response
-    response.send({
-        identity: identity,
-        token: token.toJwt()
-    });
+    // Serialize the token to a JWT string
+    response.send(token.toJwt());
 });
 
 // Create http server and run it
